5427118757e0b7356ae07cdfd057b773d3791f64
dude24760 <joke66@live.co.uk>
2015-02-20 19:52:35 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#This is both client and server combined
+
 import os
 import socket
 from random import randint
@@ -14,6 +16,7 @@ TCP_PORT = 2014
 BUFFER_SIZE = 2048
 decision = 0
 
+#Local user stats
 username = ""
 hp = 100
 level = 1
@@ -23,6 +26,7 @@ attack = randint(5, 12)
 defence = randint(1, 4)
 speed = randint(1, 10)
 
+#Opponent stats
 opponentUsername = ""
 opponentHp = 0
 opponentLevel = 0
@@ -30,6 +34,7 @@ opponentAttack = 0
 opponentDefence = 0
 opponentSpeed = 0
 
+#Processes received commands on server side
 def serverProcess(data, hp, level, xptonext, money, attack, defence, speed, opponentUsername, opponentHp, opponentLevel, opponentAttack, opponentDefence, opponentSpeed):
     cmd = data.split(" ")
     if cmd[0] == "attack":
@@ -39,6 +44,7 @@ def serverProcess(data, hp, level, xptonext, money, attack, defence, speed, oppo
         #raw_input()
         serverMain(data, hp, level, xptonext, money, attack, defence, speed, opponentUsername, opponentHp, opponentLevel, opponentAttack, opponentDefence, opponentSpeed)
 
+#Processes received commands on client side
 def clientProcess(data, hp, level, xptonext, money, attack, defence, speed, opponentUsername, opponentHp, opponentLevel, opponentAttack, opponentDefence, opponentSpeed):
     cmd = data.split(" ")
     if cmd[0] == "attack":
@@ -48,7 +54,7 @@ def clientProcess(data, hp, level, xptonext, money, attack, defence, speed, oppo
         #raw_input()
         clientMain(data, hp, level, xptonext, money, attack, defence, speed, opponentUsername, opponentHp, opponentLevel, opponentAttack, opponentDefence, opponentSpeed)
 
-
+#Main loop for client. Displays vars and asks for command.
 def clientMain(data, hp, level, xptonext, money, attack, defence, speed, opponentUsername, opponentHp, opponentLevel, opponentAttack, opponentDefence, opponentSpeed):
     #os.system("cls")
     print "Username:" + username + " HP:" + str(hp) + " Level:" + str(level) + " Attack:" + str(attack) + " Defence:" + str(defence) + " Speed:" + str(speed)
@@ -66,7 +72,7 @@ def clientMain(data, hp, level, xptonext, money, attack, defence, speed, opponen
         opponentHp = opponentHp - int(data)
         clientWait(data, hp, level, xptonext, money, attack, defence, speed, opponentUsername, opponentHp, opponentLevel, opponentAttack, opponentDefence, opponentSpeed)
 
-
+#Same as clientMain
 def serverMain(data, hp, level, xptonext, money, attack, defence, speed, opponentUsername, opponentHp, opponentLevel, opponentAttack, opponentDefence, opponentSpeed):
     print "Your turn"
     #raw_input()
@@ -75,6 +81,7 @@ def serverMain(data, hp, level, xptonext, money, attack, defence, speed, opponen
     print "Opponent:" + opponentUsername + " HP:" + str(opponentHp) + " Level:" + str(opponentLevel) + " Attack:" + str(opponentAttack) + " Defence:" + str(opponentDefence) + " Speed:" + str(opponentSpeed)
     print "Your turn."
     action = raw_input(">")
+    #Splits command which allows for more detailed commands 
     cmd = action.split(" ")
     
     if action == "help":
@@ -87,6 +94,7 @@ def serverMain(data, hp, level, xptonext, money, attack, defence, speed, opponen
         opponentHp = opponentHp - int(data)
         serverWait(data, hp, level, xptonext, money, attack, defence, speed, opponentUsername, opponentHp, opponentLevel, opponentAttack, opponentDefence, opponentSpeed)
 
+#Waits for client data(command to process). This section, as well as clientMain have bug.
 def serverWait(data, hp, level, xptonext, money, attack, defence, speed, opponentUsername, opponentHp, opponentLevel, opponentAttack, opponentDefence, opponentSpeed):
     print "Not your turn."
     #raw_input()
@@ -96,6 +104,7 @@ def serverWait(data, hp, level, xptonext, money, attack, defence, speed, opponen
     data = conn.recv(BUFFER_SIZE)
     serverProcess(data, hp, level, xptonext, money, attack, defence, speed, opponentUsername, opponentHp, opponentLevel, opponentAttack, opponentDefence, opponentSpeed)
 
+#Same as serverWait. When user presses enter on wait, both clients crash or hang.
 def clientWait(data, hp, level, xptonext, money, attack, defence, speed, opponentUsername, opponentHp, opponentLevel, opponentAttack, opponentDefence, opponentSpeed):
     print "Not your turn."
     #raw_input()
@@ -105,14 +114,21 @@ def clientWait(data, hp, level, xptonext, money, attack, defence, speed, opponen
     data = s.recv(BUFFER_SIZE)
     clientProcess(data, hp, level, xptonext, money, attack, defence, speed, opponentUsername, opponentHp, opponentLevel, opponentAttack, opponentDefence, opponentSpeed)
 
+#Inits server
 def server():
+    #Makes socket global so it can be used in other functions
     global s
+    #Creates socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #Binds to serverIp
     s.bind(("192.168.0.19", TCP_PORT))
+    #Listens for clients (100)
     s.listen(100)
     print "Waiting for the opponent(s)"
     global conn, addr
+    #Accepts client
     conn, addr = s.accept()
+    #Following 7 lines sends and receives username and stats between each other in turns.
     conn.send(username)
     opponentUsername = conn.recv(BUFFER_SIZE)
     print opponentUsername
@@ -120,12 +136,14 @@ def server():
     data = conn.recv(BUFFER_SIZE)
     status = conn.recv(BUFFER_SIZE)
     print opponentUsername + " has raised their fists against you. Fuck the greetings, time for them to get rekt."
+    #Splits stats into array for easy handling
     data = data.split(" ")
     opponentHp = int(data[0])
     opponentLevel = int(data[1])
     opponentAttack = int(data[2])
     opponentDefence = int(data[3])
     opponentSpeed = int(data[4])
+    #Client handles who starts first, this just receives the results.
     if status == "wait":
         serverWait(data, hp, level, xptonext, money, attack, defence, speed, opponentUsername, opponentHp, opponentLevel, opponentAttack, opponentDefence, opponentSpeed)
     elif status == "done":
@@ -134,7 +152,7 @@ def server():
         print status
         print "Error: Incorrect data."
 
-
+#Init for client
 def client():
     print "Enter server IP."
     TCP_IP = raw_input(">")
@@ -152,6 +170,7 @@ def client():
     opponentAttack = int(data[2])
     opponentDefence = int(data[3])
     opponentSpeed = int(data[4])
+    #Checks which of the players speed is greater, and tells server
     if speed > opponentSpeed:
         s.send("wait")
         clientMain(data, hp, level, xptonext, money, attack, defence, speed, opponentUsername, opponentHp, opponentLevel, opponentAttack, opponentDefence, opponentSpeed)
